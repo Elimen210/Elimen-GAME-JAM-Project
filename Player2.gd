ce09965b03e031c0c244bc866a1e6a75b22565d9
Elie MEN <133395047+Elimen210@users.noreply.github.com>
2024-07-10 21:51:35 +0100

    Adding of Animation commit

--- Player2.gd
+++ Player2.gd
@@ -1,17 +1,41 @@
 extends CharacterBody3D
 
-
+var mouse_sensitivity := 0.001
 const SPEED = 4.0
 const JUMP_VELOCITY = 4.5
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
-
+var lookat
+var lastLookAtDirection : Vector3
+@onready var animationtree = get_node("AnimationTree")
+var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+var twist_input := 0.0
+var pitch_input := 0.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 func _physics_process(delta):
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, 
+		deg_to_rad(-30), 
+		deg_to_rad(30)
+	)
+	twist_input = 0.0
+	pitch_input = 0.0
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY * gravity * delta
 	# Add the gravity.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
@@ -29,5 +53,13 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	$AnimationTree.set("parameters/conditions/idle", input_dir == Vector2.ZERO && is_on_floor())
+	$AnimationTree.set("parameters/conditions/walk", (input_dir.y == 1 || input_dir.y == -1) && is_on_floor())
+	$AnimationTree.set("parameters/conditions/straifLeft", input_dir.x == -1 && is_on_floor())
+	$AnimationTree.set("parameters/conditions/straifRight", input_dir.x == 1 && is_on_floor())
+	$AnimationTree.set("parameters/conditions/falling", !is_on_floor())
+	$AnimationTree.set("parameters/conditions/landed", is_on_floor())
+
 
 	move_and_slide()
