806542e788fbd3106a157cdd2cab3b621679e066
Elie MEN <133395047+Elimen210@users.noreply.github.com>
2024-07-10 23:59:23 +0100

    last commit I guess

--- Player2.gd
+++ Player2.gd
@@ -13,6 +13,8 @@ var twist_input := 0.0
 var pitch_input := 0.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var health : float = 5.0
+var damage
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -35,13 +37,18 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY * gravity * delta
 	# Add the gravity.
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("ui_enter"):
+		get_tree().change_scene_to_file("res://game_over.tscn")
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -56,10 +63,14 @@ func _physics_process(delta):
 		
 	$AnimationTree.set("parameters/conditions/idle", input_dir == Vector2.ZERO && is_on_floor())
 	$AnimationTree.set("parameters/conditions/walk", (input_dir.y == 1 || input_dir.y == -1) && is_on_floor())
-	$AnimationTree.set("parameters/conditions/straifLeft", input_dir.x == -1 && is_on_floor())
-	$AnimationTree.set("parameters/conditions/straifRight", input_dir.x == 1 && is_on_floor())
+	$AnimationTree.set("parameters/conditions/straifLeft", input_dir.x == 1 && is_on_floor())
+	$AnimationTree.set("parameters/conditions/straifRight", input_dir.x == -1 && is_on_floor())
 	$AnimationTree.set("parameters/conditions/falling", !is_on_floor())
 	$AnimationTree.set("parameters/conditions/landed", is_on_floor())
-
-
+	
 	move_and_slide()
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://node_3d.tscn")
+
+func _on_mesh_instance_3d_26_child_entered_tree(node):
+	pass # Replace with function body.
---
+++ UI.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://node_3d.tscn")
+	
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
